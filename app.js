12148c26be60ec1ae0ef4f01edbd98c5f633ccb6
fhusain01 <fhusain@adaptready.com>
2022-12-05 22:18:10 +0530

    Created basic express server and add basic route

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-console.log('Task Manager App')
+const express = require('express');
+const app = express();
+
+// routes
+app.get('/home', (req, res) => {
+    res.send('Task Manager App');
+})
+
+const port = 3000;
+app.listen(3000, console.log(`Server is listening to port ${port} ...`))
