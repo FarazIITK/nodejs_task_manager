6f622d9dabcaaf566b101adbd771eea8b827ce47
fhusain01 <fhusain@adaptready.com>
2022-12-05 22:22:56 +0530

    Added routes definiton

--- app.js
+++ app.js
@@ -8,3 +8,9 @@ app.get('/home', (req, res) => {
 
 const port = 3000;
 app.listen(3000, console.log(`Server is listening to port ${port} ...`))
+
+// app.get('/api/v1/tasks') --> get all the tasks
+// app.post('/api/v1/tasks') --> create a new task
+// app.get('/api/v1/tasks/:id') --> get single task
+// app.get('/api/v1/tasks/:id') --> update task
+// app.get('/api/v1/tasks/:id') --> delete tasks
