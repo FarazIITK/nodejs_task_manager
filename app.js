c9a21fc098d6e4458d2afafc23fb4dae1a8dc1ff
fhusain01 <fhusain@adaptready.com>
2022-12-06 22:30:14 +0530

    Added logic to start server once the db is connected

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-require('dotenv').config();
-require('./db/connect');
 const express = require('express');
 const app = express();
 const tasks = require(`./routes/tasks`)
+const connectDB = require('./db/connect');
+require('dotenv').config();
 
 // middleware
 app.use(express.json());
@@ -15,7 +15,18 @@ app.get('/home', (req, res) => {
 app.use('/api/v1/tasks', tasks);
 
 const port = 3000;
-app.listen(3000, console.log(`Server is listening to port ${port} ...`))
+
+const start = async () => {
+    try {
+        await connectDB(process.env.MONGO_URI);
+        app.listen(3000, console.log(`Server is listening on port ${port} ...`))
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+start();
+
 
 // app.get('/api/v1/tasks') --> get all the tasks
 // app.post('/api/v1/tasks') --> create a new task
--- db/connect.js
+++ db/connect.js
@@ -1,14 +1,10 @@
 const mongoose = require('mongoose');
 
-const dbName = 'TASK-MANAGER'
-
-const connectionString = `mongodb+srv://fhusain01:${process.env.PASSWORD}@taskmanagernodejsexpres.n0izri2.mongodb.net/${dbName}?retryWrites=true&w=majority`
-
 // To remove deprecation warning
 mongoose.set('strictQuery', true);
 
-mongoose.connect(connectionString).then(() => {
-    console.log(`Connected to DB ...`)
-}).catch((err) => {
-    console.log(err)
-})
+const connectDB = (connectionString) => {
+    return mongoose.connect(connectionString)
+}
+
+module.exports = connectDB;
