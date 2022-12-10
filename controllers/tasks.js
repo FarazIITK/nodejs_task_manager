2fc8b00de889b040f0497c5d6c661020dfb34c27
fhusain01 <fhusain@adaptready.com>
2022-12-10 21:31:52 +0530

    Updated response status code

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -3,7 +3,7 @@ const Task = require('../models/Task');
 const getAllTasks = async (req, res) => {
     try {
         const tasks = await Task.find({});
-        res.status(201).json({ tasks });
+        res.status(200).json({ tasks });
     } catch (error) {
         res.status(500).json({ msg: error })
     }
@@ -12,7 +12,7 @@ const getAllTasks = async (req, res) => {
 const createTask = async (req, res) => {
     try {
         const task = await Task.create(req.body);
-        res.status(201).json({ task });
+        res.status(200).json({ task });
     } catch (error) {
         res.status(500).json({ msg: error })
     }
