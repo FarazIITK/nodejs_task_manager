8b95f328c3dd60416c75f73673c0880b1fc58c75
fhusain01 <fhusain@adaptready.com>
2022-12-10 19:14:22 +0530

    Added options to return new data and run validators

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -35,7 +35,10 @@ const getTask = async (req, res) => {
 const updateTask = async (req, res) => {
     try {
         const { id: taskId } = req.params;
-        const task = await Task.findOneAndUpdate({ _id: taskId }, req.body);
+        const task = await Task.findOneAndUpdate({ _id: taskId }, req.body, {
+            new: true,
+            runValidators: true
+        });
 
         if (!task) {
             return res.status(404).json({ msg: `Can't find task with id ${taskId}` });
